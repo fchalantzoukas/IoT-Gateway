c69d4a76d2449ccb715eb3d73346d9bd72c976fd
fchalantzoukas <96824113+fchalantzoukas@users.noreply.github.com>
2023-01-09 20:39:35 +0200

    Local 3-scan test for supposed 30min speech

--- gtw.py
+++ gtw.py
@@ -8,6 +8,8 @@ class ScanDelegate(DefaultDelegate):
     def handleDiscovery(self, dev, isNewDev, isNewData):
         pass
 
+#------------------------------------------------------
+
 def filterDevices(devices):
     beacons=set()
     for dev in devices:
@@ -21,11 +23,39 @@ def printBeacons(beacons, msgTypes):
         msgType=msgTypes[msg[-8]]
         print ("Device: {}\nMAC Address: {}\nRSSI={} dB\nMessage Type: {}\n".format(beac.addr[-2:].upper(),beac.addr, beac.rssi,msgType))
 
+def scanDevices(scanner, msgTypes, testScans):
+    viewersList=set() #Total viewers
+    viewTime=dict()
+    for scan in testScans:
+        print('{} Scan\n{}'.format(scan,(len(scan)+5)*'-'))
+        viewers=doScan(scanner,msgTypes) #Single Scan viewers
+        for viewer in viewers:
+            if viewer in viewersList:
+                viewTime[viewer]+=10
+            else:
+                viewersList.add(viewer)
+                viewTime[viewer]=10
+    return viewTime
+
+#Single scan
+def doScan(scanner, msgTypes):
+    devices=scanner.scan(5, passive=True)
+    beacons=filterDevices(devices)
+    printBeacons(beacons, msgTypes)
+    viewers=set()
+    for beac in beacons:
+        viewers.add(beac.addr)
+    return viewers
+
+#------------------------------------------------------
 
 msgTypes={'1':'Exists','2':'Connection','3':'Question'}
+testScans=['First','Second','Third']
 
 scanner=Scanner().withDelegate(ScanDelegate())
-devices=scanner.scan(5, passive=True)
 
-beacons=filterDevices(devices)
-printBeacons(beacons, msgTypes)
+viewTime = scanDevices(scanner, msgTypes, testScans)
+
+print('Watchtime:\n'+10*'-')
+for viewer in viewTime:
+    print('{}: {} minutes'.format(viewer[-2:].upper(), viewTime[viewer]))
